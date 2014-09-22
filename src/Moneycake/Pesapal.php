cc474f32b54cfaa8ed4b1a65318a4d9d4f372e9f
Acellam Guy <abiccel@yahoo.com>
2014-09-22 18:28:43 +0300

    fixed xml issue

--- src/Moneycake/Pesapal.php
+++ src/Moneycake/Pesapal.php
@@ -101,7 +101,7 @@ class Pesapal {
     public function PostPesapalDirectOrderV4($callback_url, $amount, $desc, $type, $reference, $email, $phonenumber, $first_name, $last_name) {
 
         $this->url = $this->http . "pesapal.com/api/PostPesapalDirectOrderV4";
-        $post_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><PesapalDirectOrderInfo xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" Amount=\"" + $amount + "\" Description=\"" + $desc + "\" Type=\"" + $type + "\" Reference=\"" + $reference + "\" FirstName=\"" + $first_name + "\" LastName=\"" + $last_name + "\" Email=\"" + $email + "\" PhoneNumber=\"" + $phonenumber + "\" xmlns=\"http://www.pesapal.com\" />";
+        $post_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><PesapalDirectOrderInfo xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" Amount=\"".$amount."\" Description=\"".$desc."\" Type=\"".$type."\" Reference=\"".$reference."\" FirstName=\"".$first_name."\" LastName=\"".$last_name."\" Email=\"".$email."\" PhoneNumber=\"".$phonenumber."\" xmlns=\"http://www.pesapal.com\" />";
         $post_xml = h($post_xml);
 
 
